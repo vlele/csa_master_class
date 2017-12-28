a5fee21e2a570036ee3bb7f4be5d2f3991b8fc34
manishkumar-agarwal <manish.kumar.agarwal1@gmail.com>
2017-12-28 19:42:57 +0530

    Providing the uri detaila for template file and parameter file

--- IaaS Essentials/Public-IP/Public-IP.ps1
+++ IaaS Essentials/Public-IP/Public-IP.ps1
@@ -1,10 +1,24 @@
-﻿
-$resourceGroupName = 'm1-AzureIaaSEssentials1'
+﻿<#
+------------
+Description
+------------
+
+This script is used to deploy the Public-IP resources. 
+The template and the parameters are being referred from the repository as mentioned in the below variables templateUri & templateParameterUri
+
+
+#>
+
+
+$resourceGroupName = 'm1-AzureIaaSEssentials'
 
 $resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName
 
-$templateUri = ''
-$templateParameterUri = ''
+$githubHandle = 'https://raw.githubusercontent.com/manishkumar-agarwal/'
+
+$templateUri = $githubHandle + 'csa_master_class/master/IaaS%20Essentials/Public-IP/Public-IP.json'
+
+$templateParameterUri = $githubHandle + 'csa_master_class/master/IaaS%20Essentials/Public-IP/Public-IP.parameters.json'
 
 
  New-AzureRmResourceGroupDeployment -Name "PublicIPDeployment" -ResourceGroupName $resourceGroupName -Mode Incremental `
@@ -12,6 +26,7 @@ $templateParameterUri = ''
 
 
  <#
+
  ---------------------------------------------------------------------------------------------------------------------------
  Usage of the below commands is for scenarios where user wants to download the template and parameter file onto their 
  local machine and initiate template deployment from the local template source.
