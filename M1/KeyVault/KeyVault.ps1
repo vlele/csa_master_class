422bd5084718a00246fed1e2c3a748087d0f1350
manishkumar-agarwal <manish.kumar.agarwal1@gmail.com>
2018-01-29 14:39:26 +0530

    Folder structure change (#2)
    * Initial Azure Environment script
    
    * Adding template for the Public-IP deployment
    
    * Providing the uri detaila for template file and parameter file
    
    * Adding new properties for Addresstype and idletime out for Public IP
    
    * Adding the artifacts for NSG
    
    * Adding Virtual Network and Subnetwork scripts
    
    * Script to add Subnet to an existing VNET
    
    * Scripts to add Peering between VNETS
    
    * Addition of Load Balancer scripts
    
    * scripts for creation of Storage account
    
    * Scripts for adding availability Sets
    
    * scripts for application gateway
    
    * Scripts for the addition of Nics
    
    * Adding script for provisioning Virtual machines
    
    * Cleanup script for IaaSEssentials
    
    * Script for Keyvault and chnges done to VM scripts for demo using keyvault
    
    * Modifying script to allow execution to pass parameter value dynamically.
    
    * Changes for accepting parameter dynamically in the VM provisioning template and creating the VNET peering completion
    
    * Changing the base folder naming convention
    
    * Modifying the folder structure naming inside the scripts

---
+++ M1/KeyVault/KeyVault.ps1
@@ -0,0 +1,55 @@
+﻿<#
+------------
+Description
+------------
+
+This script is used to create a Key Vault and store secret values in it.
+The script will prompt for password which will be stored in Key vault and later retrived directly from the Keyvault while provisioning VMs 
+The template and the parameters are being referred from the repository as mentioned in the below variables templateUri & templateParameterUri
+
+-------------
+Dependencies
+-------------
+
+N/A
+
+#>
+
+
+$resourceGroupName = 'm1-AzureIaaSEssentials'
+
+$resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName
+
+$githubHandle = 'https://raw.githubusercontent.com/vlele/'
+
+$templateUri = $githubHandle + 'csa_master_class/master/M1/KeyVault/KeyVault.json'
+
+$templateParameterUri = $githubHandle + 'csa_master_class/master/M1/KeyVault/KeyVault.parameters.json'
+
+
+$adminpassword = Read-Host -assecurestring "Enter local admin password for VMs"
+
+$aadUser = Get-AzureRmADUser -Mail (Get-AzureRmContext).Account.Id
+
+ New-AzureRmResourceGroupDeployment -Name "KeyVault-Deployment" -ResourceGroupName $resourceGroupName -Mode Incremental `
+ -TemplateUri $templateUri  -user-object-id $aadUser.Id -adminpassword $adminpassword -Verbose
+
+
+ <#
+
+ ---------------------------------------------------------------------------------------------------------------------------
+ Usage of the below commands is for scenarios where user wants to download the template and parameter file onto their 
+ local machine and initiate template deployment from the local template source.
+
+ This would work only in cases of the main template or stand alone templates
+ ---------------------------------------------------------------------------------------------------------------------------
+
+ $adminpassword = Read-Host -assecurestring "Enter local admin password for VMs"
+
+ $aadUser = Get-AzureRmADUser -Mail (Get-AzureRmContext).Account.Id
+
+ New-AzureRmResourceGroupDeployment -Name "KeyVault-Deployment"  -ResourceGroupName 'm1-AzureIaaSEssentials' -Mode Incremental `
+ -TemplateFile "C:\Users\aisadmin\Source\Repos\csa_master_class\M1\KeyVault\KeyVault.json" `
+ -TemplateParameterFile "C:\Users\aisadmin\Source\Repos\csa_master_class\M1\KeyVault\KeyVault.parameters.json"  `
+ -user-object-id $aadUser.Id -adminpassword $adminpassword -Verbose
+ #>
