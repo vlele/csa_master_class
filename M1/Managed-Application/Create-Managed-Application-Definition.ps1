ddb10d8abff52e189dc9aad1d0fe76e42ccaec10
manishkumar-agarwal <manish.kumar.agarwal1@gmail.com>
2018-04-24 22:10:48 +0530

    Managed Application Demo (#5)
    * Removing earlier folder structure
    
    * Initial commit for M2
    
    * cloud service set-up commit
    
    * Managed Service Identity addition
    
    * Adding Managed Application zip
    
    * Managed Application Deployment scripts

---
+++ M1/Managed-Application/Create-Managed-Application-Definition.ps1
@@ -0,0 +1,62 @@
+﻿<#
+------------
+Description
+------------
+
+This script is used to create an Managed Application Definition. 
+Once the Managed Appliction Definition is created, it is present in the Service Catalog of the subscription
+
+The Managed Application being deployed is referred from the repository as mentioned in the below variables managedApplicationUri
+
+The script creates a new resource group and creates the Managed Application Definition in this resource group.
+
+Based on the user specified in the search string the managed application definition will add details so the user is added as owner when application instances are created
+
+-------------
+Dependencies
+-------------
+
+N/A
+
+#>
+
+$userEmail = Read-Host -Prompt "Input the email of the user for Role Assignment"
+
+# Get Azure Active Directory user Id to manage the application
+$userId=(Get-AzureRmADUser -Mail ($userEmail)).Id
+
+if(!$userId){
+
+    Write-Host "Entered Email does not exists in AAD. Please retry with a valid id"
+
+}
+
+else
+{
+
+    $resourceGroupName = 'm1-ManagedApplicationDefinition'
+
+    $resourceGroup = New-AzureRmResourceGroup -Name $resourceGroupName -Location "EastUS" -Force -Verbose
+
+    $githubHandle = 'https://raw.githubusercontent.com/vlele/'
+
+    $managedApplicationUri = $githubHandle + 'csa_master_class/master/M1/Managed-Application/ManagedApplication.zip'
+
+
+    # Get role
+    $roleId=(Get-AzureRmRoleDefinition -Name Owner).Id
+
+
+    # Create the definition for a managed application
+    New-AzureRmManagedApplicationDefinition `
+      -Name "m1ManagedApplication" `
+      -Location $resourceGroup.Location `
+      -ResourceGroupName $resourceGroupName `
+      -LockLevel ReadOnly `
+      -DisplayName "m1 Managed Application" `
+      -Description "This is a test Managed Application to create Azure Storage Account" `
+      -Authorization "${userId}:$roleId" `
+      -PackageFileUri $managedApplicationUri
+
+  }
+
---
+++ M1/Managed-Application/Create-Managed-Application.ps1
@@ -0,0 +1,44 @@
+﻿<#
+------------
+Description
+------------
+
+This script is used to create an Managed Application. 
+Once the Managed Appliction instance is created, it has two resource groups
+
+a) Application Resource Group (Contains managed application instance)
+b) Managed Resource Group (Contains actual resources and has RBAC assigned as per Managed Application definition)
+
+The Managed Application being deployed is referred from the already deployed Managed Application Definition
+
+-------------
+Dependencies
+-------------
+
+1) The Create-Managed-Application-Definition.ps1 script should be run before this script. This Managed Application is created from the same
+
+
+#>
+
+$resourceGroupName = 'm1-ManagedApplication'
+
+$managedResourceGroupName = $resourceGroupName + '-Managed'
+
+$resourceGroup = New-AzureRmResourceGroup -Name $resourceGroupName -Location "EastUS" -Force -Verbose
+
+# Get ID of managed application definition
+$managedAppDefinitionId=(Get-AzureRmManagedApplicationDefinition -ResourceGroupName m1-ManagedApplicationDefinition -Name m1ManagedApplication).ManagedApplicationDefinitionId
+
+
+    
+# Create the managed application
+New-AzureRmManagedApplication `
+  -Name m1storageApp `
+  -Location $resourceGroup.Location  `
+  -Kind ServiceCatalog `
+  -ResourceGroupName $resourceGroupName `
+  -ManagedApplicationDefinitionId $managedAppDefinitionId `
+  -ManagedResourceGroupName $managedResourceGroupName `
+  -Parameter "{`"storageAccountNamePrefix`": {`"value`": `"m1demo`"}, `"storageAccountType`": {`"value`": `"Standard_LRS`"}}" `
+  -Verbose
+
