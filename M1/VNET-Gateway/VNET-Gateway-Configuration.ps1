26e754f29fabc6d5b531c1fed79bf278af7df5c0
manishkumar-agarwal <manish.kumar.agarwal1@gmail.com>
2018-05-27 23:23:48 +0530

    VNET Gateway P2S Setup

---
+++ M1/VNET-Gateway/VNET-Gateway-Configuration.ps1
@@ -0,0 +1,51 @@
+﻿<#
+------------
+Description
+------------
+
+This script is used to perform the configuration for the Virtual Network Gateway
+Setting-up of the VPN Client Address Pool and Root Cerificate is all done in this script 
+The template and the parameters are being referred from the repository as mentioned in the below variables templateUri & templateParameterUri
+
+-------------
+Dependencies
+-------------
+
+1) The VNET-Gateway.ps1 script should be run before this script. This is the VNET Gateway which is configured
+
+--------------
+Inputs Needed:
+--------------
+
+1) filePathForCert --> Provide the place where the RootCertificate resides. A Self Signed certificate is also present in the github folder
+
+
+#>
+
+
+
+$filePathForCert = "C:\cert\P2SRootCert.cer"
+
+
+$resourceGroupName = 'm1-AzureIaaSEssentials'
+
+$GWName = 'm1-VNETGW'
+
+# Adding VPN Client Address pool
+
+$VPNClientAddressPool = "172.16.201.0/24"
+
+$Gateway = Get-AzureRmVirtualNetworkGateway -ResourceGroupName $resourceGroupName -Name $GWName
+
+Set-AzureRmVirtualNetworkGateway -VirtualNetworkGateway $Gateway -VpnClientAddressPool $VPNClientAddressPool
+
+#Adding the public key of the root certificate to VNET Gateway
+
+$P2SRootCertName = "P2SRootCert.cer"
+
+$cert = new-object System.Security.Cryptography.X509Certificates.X509Certificate2($filePathForCert)
+$CertBase64 = [system.convert]::ToBase64String($cert.RawData)
+$p2srootcert = New-AzureRmVpnClientRootCertificate -Name $P2SRootCertName -PublicCertData $CertBase64
+
+
+Add-AzureRmVpnClientRootCertificate -VpnClientRootCertificateName $P2SRootCertName -VirtualNetworkGatewayname $GWName -ResourceGroupName $resourceGroupName -PublicCertData $CertBase64
---
+++ M1/VNET-Gateway/VNET-Gateway.ps1
@@ -0,0 +1,49 @@
+﻿<#
+------------
+Description
+------------
+
+This script is used to create a Virtual Network Gateway
+The creation of the VNET Gateway is done in an existing GatewaySubnet for demonstraton purpose 
+The template and the parameters are being referred from the repository as mentioned in the below variables templateUri & templateParameterUri
+
+-------------
+Dependencies
+-------------
+
+1) The VNET-and-Subnet script should be run before this script. The subnet is added to an existing subnet
+2) The design details for the Virtual Network and Subnet should be present
+3) The Gateway subnet should be present
+
+#>
+
+
+$resourceGroupName = 'm1-AzureIaaSEssentials'
+
+$resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName
+
+$githubHandle = 'https://raw.githubusercontent.com/vlele/'
+
+$templateUri = $githubHandle + 'csa_master_class/master/M1/VNET-Gateway/VNET-Gateway.json'
+
+$templateParameterUri = $githubHandle + 'csa_master_class/master/M1/VNET-Gateway/VNET-Gateway.parameters.json'
+
+
+ New-AzureRmResourceGroupDeployment -Name "Add-VNET-Gateway-Deployment" -ResourceGroupName $resourceGroupName -Mode Incremental `
+ -TemplateUri $templateUri -TemplateParameterUri $templateParameterUri -Verbose
+
+
+ <#
+
+ ---------------------------------------------------------------------------------------------------------------------------
+ Usage of the below commands is for scenarios where user wants to download the template and parameter file onto their 
+ local machine and initiate template deployment from the local template source.
+
+ This would work only in cases of the main template or stand alone templates
+ ---------------------------------------------------------------------------------------------------------------------------
+
+
+ New-AzureRmResourceGroupDeployment -Name "Add-VNET-Gateway-Deployment"  -ResourceGroupName 'm1-AzureIaaSEssentials' -Mode Incremental `
+ -TemplateFile "C:\Users\aisadmin\Source\Repos\csa_master_class\M1\VNET-Gateway\VNET-Gateway.json" `
+ -TemplateParameterFile "C:\Users\aisadmin\Source\Repos\csa_master_class\M1\VNET-Gateway\VNET-Gateway.parameters.json" -Verbose
+ #>
